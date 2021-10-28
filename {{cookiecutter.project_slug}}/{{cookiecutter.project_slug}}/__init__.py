f188dd4fde1074361d212ded807030e4ae5b3ea5
Matthew Wyant <me@zanfar.com>
2021-10-28 12:01:59 -0700

    Added Flake8 tests

--- {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/__init__.py
+++ {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/__init__.py
@@ -6,6 +6,6 @@ __copyright__ = "Copyright {% now 'local', '%Y' %}, {{ cookiecutter.full_name }}
 __credits__ = [__author__]
 __license__ = "{{cookiecutter.license}}"
 __version__ = "{{ cookiecutter.project_version }}"
-__version_info__ = ({{cookiecutter.project_version | replace(".", ",")}},)
+__version_info__ = ({{cookiecutter.project_version | replace(".", ", ")}})
 __maintainer__ = __author__
 __status__ = "Prototype"
--- {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/tests/test_tests.py
+++ {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/tests/test_tests.py
@@ -2,5 +2,6 @@
 
 """Test `{{ cookiecutter.project_slug }}` package test setup."""
 
+
 def test_pytest_operation():
     assert True
